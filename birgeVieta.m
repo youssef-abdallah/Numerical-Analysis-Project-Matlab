e1799a1b1df064be45daf4df77de113f3e9685e6
mazen201 <mazon.201135@gmail.com>
2019-04-23 00:00:58 +0200

    BridgeVieta & GUI

--- birgeVieta.m
+++ birgeVieta.m
@@ -1,29 +1,56 @@
-function [roots, iterationsCount, bVector, cVector] = birgeVieta(polynomial, initialGuess, maxIterations, eps )
-
+function [max] = birgeVieta(polynomial, initialGuess, maxIterations, eps, handles)
+    tic;
     functionCoeff = sym2poly(sym(polynomial));
     x = initialGuess;
+    
+    equations = strcat('@(x)',polynomial);
+    equ = str2func(equations);
+    %Need to Zoom
+    xplot = [-10 10 10 -10];
+    fplot(equ,xplot);
+    hold on
+    xx = -10:10;
+    yy = 0;
+    plot(xx,yy*ones(size(xx)), 'k');
+    hold on
+    
+    set(handles.table, 'Data', {'-','-','-',initialGuess,'-'})
+    
     done = false;
     i = 0;
     [~, m] = size(functionCoeff);
     a = functionCoeff;
-    bVector = cell(maxIterations);
-    cVector = cell(maxIterations);
     while(~done)
         i = i + 1;
         [fx, b] = syntheticDivision(a, m, x);
         a = b;
-        bVector{i} = b;
         [fdx, c] = syntheticDivision(a, m - 1, x);
-        cVector{i} = c;
+        max= length(functionCoeff);
+        for z=1:max
+        %A     B     C   Xi+1   Ea
+            if (z == max)
+                row = {functionCoeff(z),b(z),'','' ,''};
+            else
+                row = {functionCoeff(z),b(z),c(z),'' ,''};
+            end
+            oldData = get(handles.table,'Data');
+            newData = [oldData; row];
+            set(handles.table,'Data',newData)
+        end
         xNew = x - (fx / fdx);
-        if abs(x - xNew) < eps || i > maxIterations
+        err=abs(x - xNew);
+        if  err < eps || i > maxIterations
             done = true;
         end
+        row = {'-','-','-',xNew,err};
+            oldData = get(handles.table,'Data');
+            newData = [oldData; row];
+            set(handles.table,'Data',newData)
         x = xNew;
         a = functionCoeff;
     end
-    roots = x;
-    iterationsCount = i;
+    set(handles.answer, 'String' ,x(end));
+    set(handles.time,'String',toc);
 end
 
 function[fx, b] = syntheticDivision(a, m, r)
